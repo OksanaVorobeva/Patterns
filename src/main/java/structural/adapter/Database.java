d3ee37ecce2ab092f82eef52728603152a2de9bc
Oksana <Ksanka82@yandex.ru>
2023-11-11 12:52:53 +0300

    Шаблон: Адаптер(Adapter)

---
+++ src/main/java/structural/adapter/Database.java
@@ -0,0 +1,9 @@
+package structural.adapter;
+
+public interface Database {
+    public void insert();
+    public void upDate();
+    public  void select();
+    public  void  remove();
+
+}
---
+++ src/main/java/structural/adapter/JavaApplication.java
@@ -0,0 +1,19 @@
+package structural.adapter;
+
+public class JavaApplication {
+    public void  saveObject(){
+        System.out.println("Save Java object...");
+    }
+
+    public void updateObject(){
+        System.out.println("Update Java object...");
+    }
+
+    public void loadObject(){
+        System.out.println("Loading Java object...");
+    }
+
+    public void deleteObject(){
+        System.out.println("Deleting Java object...");
+    }
+}
