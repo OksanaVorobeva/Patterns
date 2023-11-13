e0a064b02e2ad736107fbd1d30aa105c0dbff117
Oksana <Ksanka82@yandex.ru>
2023-11-13 13:25:41 +0300

    Итератор (Iterator)

---
+++ src/main/java/behavioral/iterator/JavaDeveloperRunner.java
@@ -0,0 +1,14 @@
+package behavioral.iterator;
+
+public class JavaDeveloperRunner {
+    public static void main(String[] args) {
+        String[] skills={"Java","Spring","Hibernate","Maven","PostgreSQL"};
+        JavaDeveloper javaDeveloper=new JavaDeveloper("Иванов",skills);
+        Iterator iterator = javaDeveloper.getIterator();
+        System.out.println("Developer: "+javaDeveloper.getName());
+        System.out.println("Skills: ");
+        while (iterator.hasNext()){
+            System.out.println(iterator.next().toString() +" ");
+        }
+    }
+}
