363bbabb20aee51e231b4075a14e496736204ff4
Oksana <Ksanka82@yandex.ru>
2023-11-14 09:04:38 +0300

    Наблюдатель (Observer)

---
+++ src/main/java/behavioral/observer/JobSearch.java
@@ -0,0 +1,20 @@
+package behavioral.observer;
+
+public class JobSearch {
+    public static void main(String[] args) {
+        JavaDeveloperJobSite jobSite =new JavaDeveloperJobSite();
+        jobSite.addVacancy("First Java Position");
+        jobSite.addVacancy("Second Java Position");
+
+        Observer firstSubscriber = new Subscriber("Иванов");
+        Observer secondSubscriber = new Subscriber("Петров");
+
+        jobSite.addObserver(firstSubscriber);
+        jobSite.addObserver(secondSubscriber);
+
+        jobSite.addVacancy("Third Java Position");
+
+        jobSite.removeVacancy("First Java Position");
+
+    }
+}
